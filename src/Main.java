79a18d1ca9c716687f29b9494d9769d082d2e19e
hnben <hnguyen10082000@gmail.com>
2023-06-06 14:37:58 -0700

    Adding our initial files, just a hello world example

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+/**
+ * An example program
+ *
+ * @author HUy Nguyen
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * Entry into your program
+     *
+     * @param args command line
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
